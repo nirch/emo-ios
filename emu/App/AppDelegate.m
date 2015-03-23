479a4cb6f6e2c60dd381755da3e6ade891fc7d34
Aviv Israel <aviv.israel@gmail.com>
2015-03-23 11:07:38 +0200

    - Bug fix

--- emu/App/AppDelegate.m
+++ emu/App/AppDelegate.m
@@ -67,6 +67,7 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     REMOTE_LOG(@"App lifecycle: %s", __PRETTY_FUNCTION__);
+    self.fbContext = nil;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -117,7 +118,7 @@
 // Cancel
 -(void)messengerURLHandler:(FBSDKMessengerURLHandler *)messengerURLHandler didHandleCancelWithContext:(FBSDKMessengerURLHandlerOpenFromComposerContext *)context
 {
-    self.fbContext = context;
+    self.fbContext = nil;
     [self.currentFBMSharer onFBMCancel];
 }
 
